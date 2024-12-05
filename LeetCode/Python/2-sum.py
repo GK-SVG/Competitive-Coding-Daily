4cb5e92f59f7ca3a388051cb029d084a563bac3a
gk32239 <gk32239@gmail.com>
2024-12-05 18:58:45 +0530

    Leetcode https://leetcode.com/problems/two-sum/ Best Solution

--- LeetCode/Python/2-sum.py
+++ LeetCode/Python/2-sum.py
@@ -20,3 +20,22 @@ class Solution:
 obj = Solution()
 
 print(obj.twoSum([2,7,11,15], 9))
+
+
+# Best solution on Leetcode
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, value in enumerate(nums):  # 1
+            remaining = target - nums[i]  # 2
+
+            if remaining in seen:  # 3
+                return [i, seen[remaining]]  # 4
+            else:
+                seen[value] = i  # 5
+
+
+obj2 = Solution2()
+
+print(obj2.twoSum([2,7,11,15], 9))
