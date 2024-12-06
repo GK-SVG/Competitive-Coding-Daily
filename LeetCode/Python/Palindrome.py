5dcbf85519e59c4f4eca09b1e52e5c5559dbce39
gk32239 <gk32239@gmail.com>
2024-12-06 17:00:56 +0530

    leetcode problem https://leetcode.com/problems/palindrome-number/ solution

---
+++ LeetCode/Python/Palindrome.py
@@ -0,0 +1,22 @@
+# 9. Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x == 0:
+            return True
+        
+        temp = x
+        num = ""
+        while x>0:
+            remainder = x%10
+            num += f"{remainder}"
+            x = x//10
+        if int(num) == temp:
+            return True
+        else:
+            return False
+        
+obj = Solution()
+print(obj.isPalindrome(0))
